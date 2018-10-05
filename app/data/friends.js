869a86118c4c2787dab315f9fe69ff5dd2bfb185
jordan <jgcoon24@gmail.com>
2018-10-05 18:32:03 -0500

    new repo

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+    {
+      "name" : "Darth Vader",
+      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/vader.jpg",
+      "scores" : [5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+      "name" : "Yoda",
+      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/yoda.jpg",
+      "scores" : [3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      "name" : "Han Solo",
+      "photo" : "https://jordancoon.github.io/FriendFinder/hostedimages/hansolo.jpg",
+      "scores" : [1, 1, 1, 1, 1, 1, 1]
+    }
+  ];
+  
+  module.exports = friends;
---
+++ app/data/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+      "id"   : 1,
+      "text" : "using the deathstar to detroy planets brings a smile to your face."
+    },
+    {
+      "id"   : 2,
+      "text" : "When choosing friends, your first thought is, we need a wild card."
+    },
+    {
+      "id"   : 3,
+      "text" : "On a Sunday night, you'd prefer training and lifting objects."
+    },
+    {
+      "id"   : 4,
+      "text" : "All of your friends belong to the same group."
+    },
+    {
+      "id"   : 5,
+      "text" : "Vader will make a fantastic overlord, and you look forward to serving him."
+    },
+    {
+      "id"   : 6,
+      "text" : "Your idea of a good time is being annhilated the resistence scum!"
+    },
+    {
+      "id"   : 7,
+      "text" : "Lord Vader over everything!"
+    }
+  ];
+  
+  module.exports = questions;
